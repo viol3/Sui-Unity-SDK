7703f28731d340bc7d707e19175735eca3ab3f8c
Irvin <irvsteve@gmail.com>
2024-11-16 00:00:07 +0700

    Add ZK Login implementation. #119

--- Assets/Sui-Unity-SDK/Code/Sui.Utilities/Utils.cs
+++ Assets/Sui-Unity-SDK/Code/Sui.Utilities/Utils.cs
@@ -68,62 +68,6 @@ namespace Sui.Utilities
         }
 
         /// <summary>
-        /// Finds the index of the first non-zero byte in a byte array.
-        /// </summary>
-        /// <param name="bytes">The byte array to search</param>
-        /// <returns>The index of the first non-zero byte, or -1 if all bytes are zero</returns>
-        public static int FindFirstNonZeroIndex(byte[] bytes)
-        {
-            for (int i = 0; i < bytes.Length; i++)
-            {
-                if (bytes[i] != 0)
-                {
-                    return i;
-                }
-            }
-
-            return -1;
-        }
-
-        /// <summary>
-        /// Converts a BigInteger to a byte array padded to specified width.
-        /// </summary>
-        /// <param name="num">The number to convert</param>
-        /// <param name="width">The desired width in bytes</param>
-        /// <returns>Byte array padded to specified width</returns>
-        public static byte[] ToPaddedBigEndianBytes(this BigInteger num, int width)
-        {
-            // Convert to hex string and pad
-            string hex = num.ToString("X");
-            hex = hex.PadLeft(width * 2, '0');
-
-            // Take only the last width*2 characters to match desired byte length
-            hex = hex.Substring(Math.Max(0, hex.Length - width * 2));
-
-            return HexStringToByteArray(hex);
-        }
-
-        /// <summary>
-        /// Converts a BigInteger to a big-endian byte array, removing leading zeros but ensuring at least one byte is returned.
-        /// </summary>
-        /// <param name="num">The number to convert</param>
-        /// <param name="width">The maximum width in bytes</param>
-        /// <returns>A byte array representing the number with minimal leading zeros</returns>
-        public static byte[] ToBigEndianBytes(BigInteger num, int width)
-        {
-            byte[] bytes = ToPaddedBigEndianBytes(num, width);
-
-            int firstNonZeroIndex = FindFirstNonZeroIndex(bytes);
-
-            if (firstNonZeroIndex == -1)
-            {
-                return new byte[] { 0 };
-            }
-
-            return bytes.Skip(firstNonZeroIndex).ToArray();
-        }
-
-        /// <summary>
         /// Fast check if the string to know if base58 str
         /// </summary>
         /// <param name="str"></param>
---
+++ Assets/Sui-Unity-SDK/Code/Sui.ZKLogin/Bcs.cs
@@ -0,0 +1,73 @@
+using System.Numerics;
+using Newtonsoft.Json;
+using OpenDive.BCS;
+
+namespace Sui.ZKLogin
+{
+    [JsonObject]
+    public class ZkLoginSignature: ISerializable
+    {
+        [JsonProperty("inputs")]
+        public Inputs Inputs { get; set; }
+
+        [JsonProperty("maxEpoch")]
+        public BigInteger MaxEpoch { get; set; }
+
+        [JsonProperty("userSignature")]
+        public BigInteger UserSignature { get; set; }
+
+        public void Serialize(Serialization serializer)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public static ISerializable Deserialize(Deserialization deserializer)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    [JsonObject]
+    public class Inputs
+    {
+        [JsonProperty("proofPoints")]
+        public ProofPoints proofPoints { get; set; }
+
+        [JsonProperty("issBase64Details")]
+        public ZkLoginSignatureInputsClaim IssBase64Details { get; set; }
+
+        [JsonProperty("headerBase64")]
+        public BigInteger HeaderBase64 { get; set; }
+
+        [JsonProperty("addressSeed")]
+        public BigInteger AddressSeed { get; set; }
+    }
+
+    [JsonObject]
+    public class ProofPoints
+    {
+        // Sequence input = new Sequence(new string[] { "a", "abc", "def", "ghi" }.ToList().Select(str => new BString(str)).ToArray());
+        [JsonProperty("a")]
+        public Sequence A { get; set; } // a: bcs.vector(bcs.string())
+
+        [JsonProperty("b")]
+        public Sequence B { get; set; } // b: bcs.vector(bcs.vector(bcs.string())),
+
+        [JsonProperty("c")]
+        public Sequence C { get; set; } // c: bcs.vector(bcs.string()),
+    }
+
+    [JsonObject]
+    public class ZkLoginSignatureInputsClaim
+    {
+        [JsonProperty("value")]
+        public BigInteger Value { get; set; }
+
+        [JsonProperty("indexMod4")]
+        public BigInteger IndexMod4 { get; set; }
+    }
+
+    public static class Bcs
+    {
+    }
+}
