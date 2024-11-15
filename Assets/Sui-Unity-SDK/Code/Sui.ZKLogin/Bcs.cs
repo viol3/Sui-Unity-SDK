7ed92073fd826fb87eb307eaf28dee8af0828e82
Irvin <irvsteve@gmail.com>
2024-11-16 00:59:05 +0700

    Add Zk Login Signature and dummy BCS class. #119

--- Assets/Sui-Unity-SDK/Code/Sui.ZKLogin/Bcs.cs
+++ Assets/Sui-Unity-SDK/Code/Sui.ZKLogin/Bcs.cs
@@ -5,20 +5,22 @@ using OpenDive.BCS;
 namespace Sui.ZKLogin
 {
     [JsonObject]
-    public class ZkLoginSignature: ISerializable
+    public class ZkLoginSignatureBCS: ISerializable
     {
         [JsonProperty("inputs")]
         public Inputs Inputs { get; set; }
 
         [JsonProperty("maxEpoch")]
-        public BigInteger MaxEpoch { get; set; }
+        public ulong MaxEpoch { get; set; }
 
         [JsonProperty("userSignature")]
-        public BigInteger UserSignature { get; set; }
+        public byte[] UserSignature { get; set; } // bcs.vector(bcs.u8()),
 
         public void Serialize(Serialization serializer)
         {
-            throw new System.NotImplementedException();
+            serializer.Serialize(Inputs);
+            serializer.SerializeU64(MaxEpoch);
+            serializer.SerializeBytes(UserSignature);
         }
 
         public static ISerializable Deserialize(Deserialization deserializer)
@@ -27,47 +29,64 @@ namespace Sui.ZKLogin
         }
     }
 
-    [JsonObject]
-    public class Inputs
-    {
-        [JsonProperty("proofPoints")]
-        public ProofPoints proofPoints { get; set; }
+    //[JsonObject]
+    //public class Inputs: ISerializable
+    //{
+    //    [JsonProperty("proofPoints")]
+    //    public ProofPoints ProofPoints { get; set; }
 
-        [JsonProperty("issBase64Details")]
-        public ZkLoginSignatureInputsClaim IssBase64Details { get; set; }
+    //    [JsonProperty("issBase64Details")]
+    //    public ZkLoginSignatureInputsClaim IssBase64Details { get; set; }
 
-        [JsonProperty("headerBase64")]
-        public BigInteger HeaderBase64 { get; set; }
+    //    [JsonProperty("headerBase64")]
+    //    public BigInteger HeaderBase64 { get; set; }
 
-        [JsonProperty("addressSeed")]
-        public BigInteger AddressSeed { get; set; }
-    }
+    //    [JsonProperty("addressSeed")]
+    //    public string AddressSeed { get; set; }
 
-    [JsonObject]
-    public class ProofPoints
-    {
-        // Sequence input = new Sequence(new string[] { "a", "abc", "def", "ghi" }.ToList().Select(str => new BString(str)).ToArray());
-        [JsonProperty("a")]
-        public Sequence A { get; set; } // a: bcs.vector(bcs.string())
+    //    public void Serialize(Serialization serializer)
+    //    {
+    //        serializer.Serialize(ProofPoints);
+    //        serializer.Serialize(IssBase64Details);
+    //        serializer.Serialize(HeaderBase64);
+    //        serializer.SerializeString(AddressSeed);
+    //    }
+    //}
 
-        [JsonProperty("b")]
-        public Sequence B { get; set; } // b: bcs.vector(bcs.vector(bcs.string())),
+    //[JsonObject]
+    //public class ProofPoints: ISerializable
+    //{
+    //    // Sequence input = new Sequence(new string[] { "a", "abc", "def", "ghi" }.ToList().Select(str => new BString(str)).ToArray());
+    //    [JsonProperty("a")]
+    //    public Sequence A { get; set; } // a: bcs.vector(bcs.string())
 
-        [JsonProperty("c")]
-        public Sequence C { get; set; } // c: bcs.vector(bcs.string()),
-    }
+    //    [JsonProperty("b")]
+    //    public Sequence B { get; set; } // b: bcs.vector(bcs.vector(bcs.string())),
 
-    [JsonObject]
-    public class ZkLoginSignatureInputsClaim
-    {
-        [JsonProperty("value")]
-        public BigInteger Value { get; set; }
+    //    [JsonProperty("c")]
+    //    public Sequence C { get; set; } // c: bcs.vector(bcs.string()),
 
-        [JsonProperty("indexMod4")]
-        public BigInteger IndexMod4 { get; set; }
-    }
+    //    public void Serialize(Serialization serializer)
+    //    {
+    //        serializer.Serialize(A);
+    //        serializer.Serialize(B);
+    //        serializer.Serialize(C);
+    //    }
+    //}
 
-    public static class Bcs
-    {
-    }
+    //[JsonObject]
+    //public class ZkLoginSignatureInputsClaim: ISerializable
+    //{
+    //    [JsonProperty("value")]
+    //    public string Value { get; set; }
+
+    //    [JsonProperty("indexMod4")]
+    //    public byte IndexMod4 { get; set; }
+
+    //    public void Serialize(Serialization serializer)
+    //    {
+    //        serializer.SerializeString(Value);
+    //        serializer.SerializeU8(IndexMod4);
+    //    }
+    //}
 }
