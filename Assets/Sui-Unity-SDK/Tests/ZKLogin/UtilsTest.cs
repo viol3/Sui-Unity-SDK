52beb77f81cca530d3802952ddd82ddecdacc611
Irvin <irvsteve@gmail.com>
2024-12-30 00:02:53 -0500

    Add ZKLogin util tests #119

--- Assets/Sui-Unity-SDK/Tests/ZKLogin/UtilsTest.cs
+++ Assets/Sui-Unity-SDK/Tests/ZKLogin/UtilsTest.cs
@@ -1,26 +1,36 @@
 using System;
+using System.Numerics;
 using NUnit.Framework;
-using UnityEngine;
+using Sui.ZKLogin;
 
 namespace Sui.Tests.ZkLogin
 {
     public class UtilsTest
     {
+        // TODO: Implement FindFirstNonZeroIndex
         [Test]
         public void FindFirstNonZeroIndexTest()
         {
+            byte[] bytes = null;
+            int actualIndex = ZKLogin.Utils.FindFirstNonZeroIndex(bytes);
             throw new NotImplementedException();
         }
 
+        // TODO: Implement ToPaddedBigEndianBytes
         [Test]
         public void ToPaddedBigEndianBytesTest()
         {
+            BigInteger bigInt = 0;
+            byte[] paddedBigIntBytes = bigInt.ToPaddedBigEndianBytes(20);
             throw new NotImplementedException();
         }
 
+        // TODO: Implement ToBigEndianByte
         [Test]
         public void ToBigEndianBytesTest()
         {
+            BigInteger bigInt = 0;
+            byte[] bigEndianBytes = bigInt.ToPaddedBigEndianBytes(20);
             throw new NotImplementedException();
         }
     }
